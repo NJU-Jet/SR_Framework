33527103fa8cb7657ead8ef683cdfad24016a361
NJU-Jet <151220022@smail.nju.edu.cn>
2020-09-21 01:05:38 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # All-super-resolution-models
-A generic framework which implements all famouts super-resolution models
+## A generic super-resolution framework which implements IMDN, LatticeNet, IDN, CARN, DRCN, EDSR, DRRN, LapSRN, VDSR. Updating...
+## Implement some useful functions for article figures. Like the following:
+
+* **1. generate\_best**: Compare your method with other methods and visualize the best patches.
+![compare.jpg](sr_framework/article_helper/compare.jpg)
+
+* **2. Frequency\_analysis**: Convert an image to 1-D spectral densities.
+
+![frequency.jpg](sr_framework/article_helper/frequency.jpg)
+
+* **3. relation**: Explore relations in fuse stage.(eg. torch.cat([t1, t2, t3, c4], dim=1) and then fuse them with 1x1 convolution)
+
+![relation.jpg](sr_framework/article_helper/relation.jpg)
+
+* **4. feature\_map**: Visualize feature map.(average feature maps along channel axis)
+
+![feature\_map.jpg](sr_framework/article_helper/feature_map.jpg)
+
--- sr_framework/demo.sh
+++ sr_framework/demo.sh
@@ -1,10 +1,1 @@
-#python train.py --opt options/train/base.yaml --name baseline
-#python train.py --opt options/train/DHF.yaml --name DHF_bs16ps96lr1e-4 --lr 1e-4 --ps 96 --bs 16 --gpu_ids 1 --use_chop
-#python train.py --opt options/train/DHF.yaml --name DHF_bs16ps128lr4e-4_Y --lr 4e-4 --train_Y
-#python train.py --opt options/train/IDN.yaml --name IDN_bs64ps64lr1e-4 --lr 1e-4 --bs 64 --ps 64
-#python train.py --opt options/train/IDN.yaml --name IDN_bs64ps64lr1e-4_Y --lr 1e-4 --bs 64 --ps 64 --train_Y
-#python train.py --opt options/train/EDSR.yaml --name EDSR_bs16ps96lr1e-4  --lr 1e-4 --bs 16 --ps 96 --gpu_ids 2 --use_chop
-#python train.py --opt options/train/FTN.yaml --name FTN_bs16ps96lr2e-4  --lr 2e-4 --bs 16 --ps 96 --gpu_ids 1 --use_chop
-#python train.py --opt options/train/IMDN.yaml --name IMDN_bs16ps96lr2e-4  --lr 2e-4 --bs 16 --ps 96 --gpu_ids 3 --use_chop
-#python train.py --opt options/train/IMDN.yaml --name IMDN_bs16ps96lr2e-4_noskip  --lr 2e-4 --bs 16 --ps 96 --gpu_ids 2 --use_chop
-python train.py --opt options/train/CARN.yaml --name CARN_bs16ps64lr1e-4_skip  --lr 1e-4 --bs 16 --ps 64 --gpu_ids 0 --use_chop
+python train.py --opt options/train/CARN.yaml --name CARN_bs16ps64lr1e-4  --lr 1e-4 --bs 16 --ps 64 --gpu_ids 0 --use_chop
